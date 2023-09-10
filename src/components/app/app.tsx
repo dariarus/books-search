d37020003b04782e128cf29373bec95a91234a23
dariarus <rusanova_dv@mail.ru>
2023-09-10 05:21:56 +0300

    refactor: сделать полупрозрачную подложку для хедера и задать ему фоновую картинку. Мелкие правки стилей

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -19,8 +19,6 @@ import {getBooksListBySearchParameters} from '../../services/actions/books';
 function App() {
   const searchValueState = useSelector(state => state.searchValueState);
   const searchParametersState = useSelector(state => state.searchParametersState);
-  const [categoryValue, setCategoryValue] = useState<string>('');
-  const [sortValue, setSortValue] = useState<string>('');
 
   const dispatch = useAppDispatch();
 
@@ -47,26 +45,28 @@ function App() {
   return (
     <div className={appStyles.content}>
       <header className={appStyles.header}>
-        <SearchForm/>
-        <div className={appStyles['header__drop-lists-wrap']}>
-          <DropList id="category"
-                    label="Category:"
-                    onChange={(e: ChangeEvent<HTMLSelectElement>) => handleSetCategoryValue(e)}>
-            {
-              categoryDropListOptions.map((option, index) => (
-                <option key={index} value={option.value === 'all' ? '' : option.value}>{option.value}</option>
-              ))
-            }
-          </DropList>
-          <DropList id="sort"
-                    label="Sort by:"
-                    onChange={(e: ChangeEvent<HTMLSelectElement>) => handleSetSortValue(e)}>
-            {
-              sortDropListOptions.map((option, index) => (
-                <option key={index} value={option.value}>{option.value}</option>
-              ))
-            }
-          </DropList>
+        <div className={appStyles['background-overlay']}>
+          <SearchForm/>
+          <div className={appStyles['header__drop-lists-wrap']}>
+            <DropList id="category"
+                      label="Category:"
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) => handleSetCategoryValue(e)}>
+              {
+                categoryDropListOptions.map((option, index) => (
+                  <option key={index} value={option.value === 'all' ? '' : option.value}>{option.value}</option>
+                ))
+              }
+            </DropList>
+            <DropList id="sort"
+                      label="Sort by:"
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) => handleSetSortValue(e)}>
+              {
+                sortDropListOptions.map((option, index) => (
+                  <option key={index} value={option.value}>{option.value}</option>
+                ))
+              }
+            </DropList>
+          </div>
         </div>
       </header>
       <main className={appStyles.main}>
